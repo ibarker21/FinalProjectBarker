e5cfa02115e09c0364d0499df327f10cb3a7be1c
ibarker21 <72314390+ibarker21@users.noreply.github.com>
2021-05-03 16:33:55 +0000

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,4 +1,31 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
-public class Snake {
+public class Snake extends JFrame {
 
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
 }
+
